3e6166ca2f1926fbf4ac434372f291666a9ea691
yesuuuss <jesupalma9@gmail.com>
2025-08-30 14:38:37 -0600

    modificcion nueva :c

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,17 @@
-
 const dns = require('dns');
 dns.setDefaultResultOrder('ipv4first'); 
-
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-
   ssl: { rejectUnauthorized: false },
   connectionTimeoutMillis: 5000,
   idleTimeoutMillis: 10000,
+
+  lookup: (hostname, opts, cb) => {
+    dns.lookup(hostname, { family: 4, all: false }, cb);
+  }
 });
 
 module.exports = pool;
