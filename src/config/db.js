8538842678cf368f64f55a6f9bccf68697051478
yesuuuss <jesupalma9@gmail.com>
2025-08-30 14:00:21 -0600

    db modificada

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,13 @@
 const { Pool } = require('pg');
+const dns = require('dns');
 require('dotenv').config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL, // con ?sslmode=require
-  ssl: { require: true, rejectUnauthorized: false },
+  connectionString: process.env.DATABASE_URL,        
+  ssl: { require: true, rejectUnauthorized: false }, 
+
+  lookup: (hostname, options, cb) =>
+    dns.lookup(hostname, { ...options, family: 4 }, cb),
 });
 
 module.exports = pool;
