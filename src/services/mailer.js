a21c61a2988e11082ea9859c4115afea6dd93392
yesuuuss <jesupalma9@gmail.com>
2025-09-06 19:26:27 -0600

    fi logs

--- src/services/mailer.js
+++ src/services/mailer.js
@@ -1,17 +1,20 @@
 const sql = require('mssql');
 require('dotenv').config(); // Solo una vez al principio
+console.log('DB_SERVER:', process.env.DB_SERVER);
+
 
 // Configuración de la conexión a SQL Server usando las variables de entorno
 const config = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  server: process.env.DB_SERVER, // Nombre del servidor o IP de la base de datos
+  server: process.env.DB_SERVER,  // Este debe ser un string válido
   database: process.env.DB_DATABASE,
   options: {
-    encrypt: true, // Para conexiones seguras, por ejemplo con Azure
-    trustServerCertificate: true, // Puedes deshabilitar esto en producción si es necesario
+    encrypt: true,
+    trustServerCertificate: true,
   }
 };
+
 
 // Conexión a la base de datos
 sql.connect(config)
